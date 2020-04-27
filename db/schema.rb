63291d1f811b20ea22fa5961a53fd92ab8c37020
Elisabeth Palander <epalander@gmail.com>
2020-04-27 16:56:20 +0200

    schema change

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_04_27_143851) do
 
-
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_143851) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.string "tagline"
@@ -46,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_143851) do
     t.index ["adventure_id"], name: "index_reviews_on_adventure_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -60,8 +56,6 @@ ActiveRecord::Schema.define(version: 2020_04_27_143851) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "reviews", "adventures"
   add_foreign_key "reviews", "users"
-
 end
