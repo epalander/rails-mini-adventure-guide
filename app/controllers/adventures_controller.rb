34115e5760afd8715ce273844aff1947c0e408fb
Joyce Kao <joyceykao@gmail.com>
2020-04-28 15:05:19 +0200

    copy/pasted over show, new ,create, edit, update, and adventure_params methods from pundit-setup branch

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -10,4 +10,40 @@ class AdventuresController < ApplicationController
       }
     end
   end
+
+  def show
+    @adventure = Adventure.find(params[:id])
+  end
+
+  def new
+    @adventure = Adventure.new
+  end
+
+  def create
+    @adventure = Adventure.new(adventure_params)
+    if @adventure.save
+      redirect_to adventure_path(@adventure)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @adventure = Adventure.find(params[:id])
+  end
+
+  def update
+    @adventure = Adventure.find(params[:id])
+    if @adventure.update(adventure_params)
+        redirect_to adventure_path(@adventure), notice: 'Adventure was successfully updated.'
+      else
+        render :edit
+      end
+  end
+
+  private
+
+  def adventure_params
+    params.require(:adventure).permit(:title, :description, :category, :distance, :stroller_friendly, :youngest_age, :difficulty, :parking, :public_transport, :directions, :address, photos: [])
+  end
 end
