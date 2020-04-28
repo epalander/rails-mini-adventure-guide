eda87f5f65b5043db83eba96e68ed5c18c4d8ebb
Sue Rocha <swissbusybee@gmail.com>
2020-04-28 13:30:51 +0200

    Added index top adventures TBC

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -1,4 +1,9 @@
 class Adventure < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+  has_many :reviews
+
+  def avg_rating
+    self.reviews.average(:rating)
+  end
 end
