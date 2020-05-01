6ad4b5f880ec8142a86a43505b518eec117619f2
Joyce Kao <joyceykao@gmail.com>
2020-05-01 20:14:23 +0200

    save changes to see what is in master

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -32,9 +32,6 @@ class AdventuresController < ApplicationController
   def show
     @adventure = Adventure.find(params[:id])
 
-    authorize @adventure
-
-
     @age = ["under < 1 year", "1-3 years", "4-6 years", "7-11 years", "12-15 years", "16+ years"]
 
     @stroller = @adventure.stroller_friendly ? 'Stroller friendly' : 'Not for strollers'
