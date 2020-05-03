e8010d2ec3506a2f63b68f09efbd5ed2ecc17781
Elisabeth Palander <epalander@gmail.com>
2020-05-03 15:56:21 +0200

    reviews are ordered by creation date. newest first.

--- app/controllers/adventures_controller.rb
+++ app/controllers/adventures_controller.rb
@@ -31,6 +31,7 @@ class AdventuresController < ApplicationController
   def show
     @adventure = Adventure.find(params[:id])
 
+    # show right value in the icon overview on the show page
     @age = ["under < 1 year", "1-3 years", "4-6 years", "7-11 years", "12-15 years", "16+ years"]
     @level = ["Easy", "Moderate", "Challenging"]
     @stroller = @adventure.stroller_friendly ? 'Stroller friendly' : 'Not for strollers'
@@ -45,6 +46,10 @@ class AdventuresController < ApplicationController
         lat: @adventure.latitude,
         lng: @adventure.longitude
       }
+
+    # newest review shown on top
+    @newest_review_first = @adventure.reviews.order(created_at: :desc)
+
   end
 
   def new
