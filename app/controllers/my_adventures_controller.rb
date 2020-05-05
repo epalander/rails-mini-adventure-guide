8b8898005baecdb0ca1b43f475350a71b5887491
Joyce Kao <joyceykao@gmail.com>
2020-05-05 12:25:42 +0200

    save here to move to another story

--- app/controllers/my_adventures_controller.rb
+++ app/controllers/my_adventures_controller.rb
@@ -1,2 +1,24 @@
 class MyAdventuresController < ApplicationController
+  def index
+  end
+
+  def new
+    @adventure = Adventure.find(params[:adventure_id])
+    @my_adventure = MyAdventure.new
+  end
+
+  def create
+    @my_adventure = My_Adventure.new
+    @my_adventure.user_id = current_user.id
+    @my_adventure.adventure_id = @adventure.id
+    if @my_adventure.save
+      redirect_to adventure_path(@adventure)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
 
   resources :adventures do
     resources :reviews, only: [:new, :create]
+    resources :my_adventures, only: [:new, :create, :destroy, :index]
   end
     resources :reviews, only: [:edit, :update, :destroy]
 
   get "/search" => "adventures#search"
 
-  resources :my_adventures, only: [:new, :create, :destroy, :index]
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
