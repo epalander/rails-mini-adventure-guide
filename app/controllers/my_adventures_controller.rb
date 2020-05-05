d597f5ae6002f80baad6e255f55634026e9abbf4
Joyce Kao <joyceykao@gmail.com>
2020-05-05 17:00:16 +0200

    added containers to index of my adventures

--- app/controllers/my_adventures_controller.rb
+++ app/controllers/my_adventures_controller.rb
@@ -1,7 +1,7 @@
 class MyAdventuresController < ApplicationController
   def index
     @myadventures = MyAdventure.where(user_id: current_user)
-    @done_adventures = @adventures.select{ |myadv| myadv[:done] == true }
-    @saved_adventures = @adventures.select{ |myadv| myadv[:done] == false }
+    @done_adventures = @myadventures.select{ |myadv| myadv[:done] == true }
+    @saved_adventures = @myadventures.select{ |myadv| myadv[:done] == false }
   end
 end
