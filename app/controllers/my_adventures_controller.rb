070340f0e30268ccda6a7d852d69a797ab7368f0
Joyce Kao <joyceykao@gmail.com>
2020-05-05 16:13:22 +0200

    save progress here on index page

--- app/controllers/my_adventures_controller.rb
+++ app/controllers/my_adventures_controller.rb
@@ -1,5 +1,7 @@
 class MyAdventuresController < ApplicationController
   def index
-    @myadventures = MyAdventure.where(user_id: current_user.id)
+    @myadventures = MyAdventure.where(user_id: current_user)
+    @done_adventures = @adventures.select{ |myadv| myadv[:done] == true }
+    @saved_adventures = @adventures.select{ |myadv| myadv[:done] == false }
   end
 end
