4c217964d271e5a2268bae37d9e6cf63f6ef81da
Sue Rocha <swissbusybee@gmail.com>
2020-05-06 14:55:56 +0200

    Added original logic

--- app/controllers/my_adventures_controller.rb
+++ app/controllers/my_adventures_controller.rb
@@ -1,8 +1,8 @@
 class MyAdventuresController < ApplicationController
   def index
     @myadventures = MyAdventure.where(user_id: current_user)
-    @saved_adventures = MyAdventure.all
-    @done_adventures = MyAdventure.where(:done == true)
+    @saved_adventures = @myadventures.select{ |myadv| myadv[:done] == false }
+    @done_adventures = @myadventures.select{ |myadv| myadv[:done] == true }
   end
 
   def new
