a4e264f958bbaf20031769dd246881c544a613d8
Joyce Kao <joyceykao@gmail.com>
2020-05-19 19:42:54 +0200

    calcuated average duration from reviews in adventure model and carried through the results to controller and also implemented it in show pages

--- app/models/adventure.rb
+++ app/models/adventure.rb
@@ -38,9 +38,41 @@ class Adventure < ApplicationRecord
         avg_difficulty: level[(self.difficulty + self.reviews.sum(:difficulty)).fdiv(self.reviews.count + 1).round],
         avg_age: age[(self.youngest_age + self.reviews.sum(:youngest_age)).fdiv(self.reviews.count + 1).round],
         num_ratings: self.reviews.count,
-        avg_duration: 0 ##program this later...
+        avg_duration: calc_avg_duration(self)
       }
     end
+  end
+
+  def calc_avg_duration(adventure)
+    avg_duration =(adventure.avg_duration + adventure.reviews.sum(:duration)).fdiv(adventure.reviews.count + 1).round
+
+    case avg_duration
+      #[[15, '15 mins'], [30, '30 mins'], [45, '45 mins'], [60, '1 hr'], [90, '1 hr 30 mins'], [120, '2 hrs'], [150, '2 hrs 30 mins'], [180, '3 hrs'], [210, '3 hrs 30 mins'], [240, '4 hrs'], [270, '4 hrs 30 mins'], [300, '5 hrs+']]
+      when 15..23
+        '15 mins'
+      when 24..37
+        '30 mins'
+      when 38..52
+        '45 mins'
+      when 53..75
+        '1 hr'
+      when 76..105
+        '1 hr 30 mins'
+      when 106..135
+        '2 hrs'
+      when 136..165
+        '2 hrs 30 mins'
+      when 166..195
+        '3 hrs'
+      when 196..225
+        '3 hrs 30 mins'
+      when 226..255
+        '4 hrs'
+      when 256..285
+        '4 hrs 30 mins'
+      else
+        '5 hrs+'
+      end
   end
 
   def rand_n(n, max)
