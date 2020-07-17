1ee093ccbd3fd0507e7757349745a6c565679c48
Joyce Kao <joyceykao@gmail.com>
2020-07-17 12:15:09 +0200

    attempt at making username unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   has_many :reviews
 
-  validates :username, uniqueness: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+   devise :database_authenticatable, :registerable,
+     :recoverable, :rememberable, :trackable
+
+  validates_presence_of     :email # optional
+  validates_uniqueness_of   :email # required
+  validates_presence_of     :username # required
+  validates_uniqueness_of   :username # required
+  validates_presence_of     :password, if: :password_required? # recommended
+  validates_confirmation_of :password, if: :password_required? # recommended
+  validates_length_of       :password, within: 6..128, allow_blank: true # recommended
 end
