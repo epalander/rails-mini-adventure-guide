55244d373d84fb9d51d61f1536cfba7bc93e24d0
Sue Rocha <swissbusybee@gmail.com>
2020-11-14 14:32:33 +0100

    installed react rails in app

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -36,3 +36,7 @@ document.addEventListener('turbolinks:load', () => {
   initMapbox();
   initAutocomplete();
 });
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
