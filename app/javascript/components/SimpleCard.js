db77caf15d061bff96b24bfbf5ba228f39fc69ee
Sue Rocha <swissbusybee@gmail.com>
2021-01-08 23:41:07 +0100

    Updated Simple Card component to render cards

--- app/javascript/components/SimpleCard.js
+++ app/javascript/components/SimpleCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
@@ -23,7 +24,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function SimpleCard() {
+export default function SimpleCard (props) {
   const classes = useStyles();
   const bull = <span className={classes.bullet}>•</span>;
 
@@ -31,7 +32,7 @@ export default function SimpleCard() {
     <Card className={classes.root}>
       <CardContent>
         <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day {this.props.something}
+          Word of the Day {props.something}
         </Typography>
         <Typography variant="h5" component="h2">
           be{bull}nev{bull}o{bull}lent
