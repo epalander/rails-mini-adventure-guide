79e04cad8f778d254ca89aac34f28db38b5b8f3a
Joyce <52547142+joyceykao@users.noreply.github.com>
2021-04-17 14:13:11 +0200

    credentials updated for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => "miniadventureguide@gmail.com",
-    :password             => <password>,
+    :user_name            => ENV["MAIL_USERNAME"],
+    :password             => ENV["MAIL_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
